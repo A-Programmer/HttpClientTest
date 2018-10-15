974f5abcacc54b8b81662e6e09417565399cad35
Kamran Sadin <mrsadin@gmail.com>
2018-10-15 09:33:38 +0330

    Init

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        //Before
+        // public static void Main(string[] args) 
+        // {
+        //     Console.WriteLine("Starting connections");
+        //     for(int i = 0; i<10; i++)
+        //     {
+        //         using(HttpClient client = new HttpClient())
+        //         {
+        //             var result = client.GetAsync("https://microsoft.com").Result;
+        //             Console.WriteLine(result.StatusCode);
+        //         }
+        //     }
+        //     Console.WriteLine("Connections done");
+        // }
+
+        //After
+        private static HttpClient Client = new HttpClient();
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Starting connections");
+            for(int i = 0; i<10; i++)
+            {
+                var result = Client.GetAsync("https://microsoft.com").Result;
+                Console.WriteLine(result.StatusCode);
+            }
+            Console.WriteLine("Connections done");
+        }
+
+        
+    }
+}
